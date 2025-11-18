a35b784b2c068725072d139ad3dfe3a455d1a427
somandhir <somandhir@gmail.com>
2025-11-18 23:04:46 +0530

    carousel-auto-loop

---
+++ carousel-app/src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Carousel from "./Carousel";
+
+function App() {
+  return <Carousel />;
+}
+
+export default App;
---
+++ carousel-app/src/Carousel.jsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from "react";
+
+function Carousel() {
+  const [data, setdata] = useState([]);
+  const [val, setval] = useState(0);
+  const [run, setRun] = useState(true);
+  useEffect(() => {
+    fetch("https://picsum.photos/v2/list?limit=100")
+      .then((res) => res.json())
+      .then((res) => {
+        setdata(res);
+      });
+  }, []);
+  useEffect(() => {
+    if (!run || data.length === 0) return;
+    const interval = setInterval(() => {
+      setval((v) => (v === data.length - 1 ? 0 : v + 1));
+    }, 2000);
+    return () => {
+      clearInterval(interval); // *** IMPORTANT
+    };
+  }, [run, data]);
+  let handleClick = (str) => {
+    console.log(str);
+
+    if (str === "l") {
+      if (val == 0) return;
+      setval((prev) => prev - 1);
+    } else {
+      console.log(val, data.length);
+
+      if (val == data.length - 1) setval(0);
+      else setval((prev) => prev + 1);
+    }
+  };
+
+  return (
+    <>
+      {data.length ? (
+        <div className="container">
+          <button
+            className="arrow left"
+            onMouseEnter={() => setRun(false)}
+            onMouseLeave={() => setRun(true)}
+            onClick={() => handleClick("l")}
+          >
+            {"<"}
+          </button>
+          <img
+            src={data[val].download_url}
+            alt=""
+            onMouseEnter={() => setRun(false)}
+            onMouseLeave={() => setRun(true)}
+          />
+          <button
+            className="arrow right"
+            onMouseEnter={() => setRun(false)}
+            onMouseLeave={() => setRun(true)}
+            onClick={() => handleClick("r")}
+          >
+            {">"}
+          </button>
+        </div>
+      ) : (
+        <div>Loading...</div>
+      )}
+    </>
+  );
+}
+
+export default Carousel;
